6dbb873bd6f7094fcc3b30c765d3b81e9b0c9e43
radik <rdceb@gmail.com>
2020-01-03 22:09:52 +0100

    inizio

---
+++ Solve_For_X.py
@@ -0,0 +1,13 @@
+
+
+"""
+link: https://www.codewars.com/kata/59c2e2a36bddd2707e000079
+
+kata's name: Solvwe For X
+
+"""
+
+def solve_for_x(equation):
+    pass
+    # print(solve_for_x('x - 5 = 20'), 25)
+    # print(solve_for_x('20 = 5 * x - 5'), 5)

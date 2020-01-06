dce763b17f98a5283490800d300b66a2011607c7
radik <rdceb8@gmail.com>
2020-01-06 10:25:01 +0100

    modificato solo ul file bash

--- bsh.sh
+++ bsh.sh
@@ -4,8 +4,9 @@
 
 git add .
 git commit -m "uguale si puo mettere sia da una parte che dallaltra"
-
-git push -u origin
+#git remote add moto2 https://github.com/radik2019/kata.git
+git push --set-upstream origin moto2
+#git push -u origin
 git status
 
 
